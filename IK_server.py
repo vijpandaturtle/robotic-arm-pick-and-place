8e01006032ade16f6e2d11a7934eb03e9a1b567b
Vijayasri Iyer <thisisvij98@gmail.com>
2017-07-14 17:21:02 +0530

    Added hoogenous tranform matrix for each frame

--- IK_server.py
+++ IK_server.py
@@ -33,35 +33,49 @@ def handle_calculate_IK(req):
 
             # Define DH param symbols
             q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+            q2 = q2 - pi/2
             d1, d2, d3, d4, d5, d6, d7 = symbols('d1:7')
             alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6= symbols('alpha0:7')
             a0, a1, a2, a3, a4, a5, a6 = symbols('a0:6')
-            
+
            # Joint angle symbols
            # Modified DH params
-           q1, q2, q3, q4, q5, q6, q7 = 
-           d1, d2, d3, d4, d5, d6, d7 = 
-           alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = 0, -pi/2, 0, -pi/2, pi/2, -pi/2, 0
-           a0, a1, a2, a3, a4, a5, a6 = 
-            
-             
+           #d1, d2, d3, d4, d5, d6, d7 = 0.33, 0, 0, 1.50, 0, 0, 0.303
+           #alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = 0, -pi/2, 0, -pi/2, pi/2, -pi/2, 0
+           #a0, a1, a2, a3, a4, a5, a6 = 0, 0.35, 1.25, -0.054, 0, 0, 0
+
+            s = { d1 : 0.33, alpha0 : 0, a0 : 0,
+                 d2 : 0, alpha1: -pi/2, a1 : 0.35,
+                 d3 : 0, alpha2 : 0, a2 : 1.25,
+                 d4 : 1.50, alpha3 : -pi/2, a3 : -0.054,
+                 d5 : 0, alpha4 : pi/2, a4 : 0,
+                 d6 : 0, alpha5 : -pi/2, a5 : 0,
+                 d7 : 0.303, alpha6 : 0, a6 : 0
+                 }
+
+
             # Define Modified DH Transformation matrix
-            function transform(q , d, a, alpha):
-                 transform = Matrix([ [cos(q), -sin(q), 0, a], [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(q)*d], [sin(q)*sin(alpha), cos(q)*sin(alpha), 
+            def  transform(q , a, d, alpha):
+                 transform = Matrix([ [cos(q), -sin(q), 0, a], [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(q)*d], [sin(q)*sin(alpha), cos(q)*sin(alpha),
                           cos(alpha), cos(alpha)*d], [0, 0, 0, 1] ])
-                 return transform 
+                 return transform
 
-
-            # Create individual transformation matrices
-            T0_1 = transform(q1, d1, a0, alpha0)
-            T1_2 = transform(q2, d2, a1, alpha1)
-            T2_3 = transform(q3, d3, a2, alpha2)
-            T3_4 = transform(q4, d4, a3, alpha3)
-            T4_5 = transform(q5, d5, a4, alpha4)
-            T5_6 = transfrom(q6, d6, a5, alpha5)
-            T6_G = transform(q7, d7, a6, alpha6)
-            
-            # Extract end-effector position and orientation from request
+          # Create individual transformation matrices
+            T0_1 = transform(q1, a0, d1, alpha0)
+            T0_1 = T0_1.subs(s)
+            T1_2 = transform(q2, a1, d2, alpha1)
+            T1_2 = T1_2.subs(s)
+            T2_3 = transform(q3, a2, d3, alpha2)
+            T2_3 = T2_3.subs(s)
+            T3_4 = transform(q4, a3, d4, alpha3)
+            T3_4 = T1_2.subs(s)
+            T4_5 = transform(q5, a4, d5, alpha4)
+            T4_5 = T4_5.subs(s)
+            T5_6 = transform(q6, a5, d6, alpha5)
+            T5_6 = T5_6.subs(s)
+            T6_G = transform(q7, a6, d7, alpha6)
+            T6_G = T6_G.subs(s)
+        # Extract end-effector position and orientation from request
 	    # px,py,pz = end-effector position
 	    # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
@@ -71,10 +85,10 @@ def handle_calculate_IK(req):
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
-     
+
             # Calculate joint angles using Geometric IK method
-            T0_G = simplify(T0_1*T1_2*T2_3*T3_4*T4_5*T5_6*T6_G)
-		
+
+
 
 
             # Populate response for the IK request
